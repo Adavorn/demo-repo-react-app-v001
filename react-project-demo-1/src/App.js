ed35f9ca6c46db8493764bdde0eb8ffc1c294f4c
adavorn <jacob46king@gmail.com>
2025-03-24 13:56:06 -0500

    added react

--- react-project-demo-1/src/App.js
+++ react-project-demo-1/src/App.js
@@ -1,26 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react" 
+
+import Header from "./components/header"
+import Footer from "./components/footer"
 
 function App() {
+
+  const [strMsg, setStrMsg] = useState("")
+
+  const divdisplay = useRef("") 
+
+  function lnkdemo1()
+  {
+    let msgText = "# App:: lnkdemo1"
+    console.log(msgText)
+ 
+    divdisplay.current.innerText = msgText
+    
+    setStrMsg(msgText)
+  }
+
+  const lnkdemo2 = () =>
+  {
+    let msgText = "# App:: lnkdemo2"
+    console.log(msgText)
+    divdisplay.current.innerText = msgText
+
+    setStrMsg(msgText)
+    
+  }
+
+  const btndemo1 = () =>
+  {
+    let msgText = "# App:: btndemo1"
+    console.log(msgText)
+    divdisplay.current.innerText = msgText
+
+    setStrMsg(msgText)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>live update ..1 </p>
-        <p>live update ..2 </p>
-      </header>
-    </div>
+<>
+      <div>
+          <Header title="React Demo app v02.0.2"/>
+          <p></p>
+          {strMsg}
+          <p></p>
+          <a href="#" onClick={lnkdemo1}>link demo 1</a>
+          <p></p>
+          <a href="#" onClick={lnkdemo2}>link demo 2</a>
+          <p></p>
+          <button onClick={btndemo1}>button demo 1</button>
+          <p></p>
+          <hr></hr>
+          <a href="#">useRef Demo 2</a>
+          <p></p>
+          <a href="#">useState Demo 2</a>
+          <p></p>
+          <Footer title="footer" year={2025}></Footer>
+      </div>
+      <div id="divdisplay" ref={divdisplay}>
+      </div>
+    </>
   );
 }
 
