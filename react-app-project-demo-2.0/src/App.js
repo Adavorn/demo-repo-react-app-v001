2d5345de358119434b6b8a9a1d31bbaa604f4711
adavorn <jacob46king@gmail.com>
2025-03-25 13:31:59 -0500

    added navbar

--- react-app-project-demo-2.0/src/App.js
+++ react-app-project-demo-2.0/src/App.js
@@ -1,15 +1,19 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {HashRouter, Routes, Route} from "react-router-dom"
 import Home from "./components/Home";
+import About from "./components/About";
+import NavBar from "./components/NavBar";
 
 function App() {
   
   return (
     <>
-      <BrowserRouter>            
-      <Routes>      
-        <Route path="/" element={<Home/>}/>
-      </Routes>
-    </BrowserRouter>    
+      <HashRouter> 
+        <NavBar/>         
+        <Routes>      
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About />}/>
+        </Routes>
+    </HashRouter>    
     </>
   );
 }
