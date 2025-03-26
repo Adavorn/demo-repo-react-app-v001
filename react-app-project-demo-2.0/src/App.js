4863c9d3ae46722171f42e099ca7315f8889ba45
adavorn <jacob46king@gmail.com>
2025-03-26 16:48:49 -0500

    added navbar

--- react-app-project-demo-2.0/src/App.js
+++ react-app-project-demo-2.0/src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Page1 from "./components/Page1";
 import NavBar from "./components/NavBar";
-9
+
 function App() {
   
   return (
